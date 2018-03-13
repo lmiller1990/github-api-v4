5d6d7c318d8fbada9953a60a545f3c1f138953aa
lmiller1990 <lachlan.miller.1990@outlook.com>
2018-03-14 01:22:15 +0900

    Add example query to get issues

--- config.js
+++
@@ -1,1 +0,0 @@
-export const token = '7c97df872e9fc10574acf9a0407754b3ddc59b76'
--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 const axios = require('axios')
+const token = require('./token')
+console.log(token)
 
-axios.post('https://api.github.com/graphql', {
+axios({
+  method: 'post',
+  url: 'https://api.github.com/graphql', 
+  data: {
+    'query': `
+    query { 
+      repository(owner: "branu-ws", name: "flamingo") {
+        issues(last: 5, states:OPEN) {
+          edges {
+            node {
+              title
+              url
+              labels(first:5) {
+                edges {
+                  node {
+                    name
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }`
+  },
   headers: {
-    `Authorization: bearer ${token}`
+    'Authorization': `bearer ${token}`
   }
 })
+  .then(res => {
+    const rep = res.data.data.repository
+    console.log(rep.issues.edges)
+  })
